837f047533560839ef8eeb040fe86540adf4c003
Christoph Kassen <c.kassen@telekom.de>
2013-04-10 12:09:55 +0200

    Fix duplicate definition problems
    These values are defined related to the quantum setup, so put them into
    the quantum manifest.

--- manifests/compute/quantum.pp
+++ manifests/compute/quantum.pp
@@ -6,5 +6,7 @@ class nova::compute::quantum (
     'libvirt_vif_driver':             value => $libvirt_vif_driver;
     #'libvirt_vif_driver':             value => 'nova.virt.libvirt.vif.LibvirtHybirdOVSBridgeDriver';
     'libvirt_use_virtio_for_bridges': value => 'True';
+    'libvirt_ovs_bridge':             value => 'br-int';
+    'libvirt_vif_type':               value => 'ethernet';
   }
 }
