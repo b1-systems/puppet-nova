d110ea94ed46030fb96ad68255ea519ef1861657
Christoph Kassen <c.kassen@telekom.de>
2013-04-10 15:02:20 +0200

    Fix a really stupid typo
    Hybird => Hybrid

--- manifests/compute/quantum.pp
+++ manifests/compute/quantum.pp
@@ -1,10 +1,10 @@
 class nova::compute::quantum (
-  $libvirt_vif_driver = 'nova.virt.libvirt.vif.LibvirtHybirdOVSBridgeDriver'
+  $libvirt_vif_driver = 'nova.virt.libvirt.vif.LibvirtHybridOVSBridgeDriver'
 ){
 
   nova_config {
     'DEFAULT/libvirt_vif_driver':             value => $libvirt_vif_driver;
-    #'libvirt_vif_driver':             value => 'nova.virt.libvirt.vif.LibvirtHybirdOVSBridgeDriver';
+    #'libvirt_vif_driver':             value => 'nova.virt.libvirt.vif.LibvirtHybridOVSBridgeDriver';
     'DEFAULT/libvirt_use_virtio_for_bridges': value => 'True';
     'DEFAULT/libvirt_ovs_bridge':             value => 'br-int';
     'DEFAULT/libvirt_vif_type':               value => 'ethernet';
